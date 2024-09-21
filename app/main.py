a3cec8032724ee2943694b056908e3b2cf98685f
rosen ritter <ch7cly@gmail.com>
2024-09-21 19:36:31 +0300

    x

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from app.routes import app1
+from app.routes import app1, app2
 
 app = FastAPI()
 app.include_router(app1.router)
+app.include_router(app2.router)
 
--- app/routes/app1.py
+++ app/routes/app1.py
@@ -4,26 +4,26 @@ from app.models.models import *
 
 router = APIRouter()
 
-@router.get("/")
+@router.get("/v1")
 async def root():
     return {"message": "Hello World"}
 
 
-@router.get("/index.html")
+@router.get("/v1/index.html")
 async def index():
     return FileResponse('./app/index.html')
 
-@router.get('/calculate/{item_id}')
+@router.get('/v1/calculate/{item_id}')
 async def calculate(item_id: str):
     a, b = map(int, item_id.split('+'))
     return {'result': a + b}
 
-@router.get("/custom")
+@router.get("/v1/custom")
 def read_custom_message():
     return {"message": "This is a custom message!"}
 
 
-@router.post("/app1")
+@router.post("/v1/app1")
 async def root(user: User):
     '''тут мы можем с переменной user, которая в себе содержит объект класса User с соответствующими полями (и указанными типами), делать любую логику
     - например, мы можем сохранить информацию в базу данных
@@ -32,13 +32,13 @@ async def root(user: User):
     print(f'Мы получили от юзера {user.username} такое сообщение: {user.message}') # тут мы просто выведем полученные данные на экран в отформатированном варианте
     return user # или можем вернуть обратно полученные данные, как символ того, что данные получили, или другая логика на ваш вкус
 
-@router.get('/users')
+@router.get('/v1/users')
 async def get_user():
     params = {'username':'serega', 'message':'chernovar', 'id':'123',}
     user: User = User(**params)
     return user
 
-@router.post('/user_is_adult')
+@router.post('/v1/user_is_adult')
 async def is_audlt(user: User1):
     class ExtendedUser(User1):
         is_audlt: bool = True
@@ -50,7 +50,7 @@ async def is_audlt(user: User1):
 feedback_db:list[Feedback] = [Feedback(username='kek', fb_message='wmek'),
                               Feedback(username='chpok', fb_message='aaaaa')]
 
-@router.get('/feedback/show/{number}')
+@router.get('/v1/feedback/show/{number}')
 async def show_feedback(number:int, show_name:bool=True):
     fb = {}
     for idx, letter in enumerate(feedback_db):
@@ -59,7 +59,7 @@ async def show_feedback(number:int, show_name:bool=True):
         fb[idx] = letter if show_name else {'fb_message':letter.fb_message}
     return fb
 
-@router.post('/feedback/write')
+@router.post('/v1/feedback/write')
 async def feedback(letter:Feedback):
     feedback_db.append(letter)
     return {'message': f'Feedback received. Thank you, {letter.username}!'}
